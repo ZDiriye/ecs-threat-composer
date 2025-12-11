c21be4829b610cc9b7315a308b3f4a4096262b4c
ZDiriye <zakariyediriye8@gmail.com>
2025-12-11 19:49:42 +0000

    Added S3 backend

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    bucket = "tfstate-ecs-threat-composer"
+    key    = "infra/terraform.tfstate"
+    region = "eu-west-2"
+    encrypt = true
+  }
+
   required_version = "~> 1.14"
 
   required_providers {
