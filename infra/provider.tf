a2d82f8e270b6ea0eaa062da21bb817fe19e51fb
ZDiriye <zakariyediriye8@gmail.com>
2025-12-11 19:58:41 +0000

    Fixed error in Terraform apply and destroy workflows

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "tfstate-ecs-threat-composer"
-    key    = "infra/terraform.tfstate"
-    region = "eu-west-2"
+    bucket  = "tfstate-ecs-threat-composer"
+    key     = "infra/terraform.tfstate"
+    region  = "eu-west-2"
     encrypt = true
   }
 
